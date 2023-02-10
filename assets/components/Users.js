9a6650ad6ed03b7867525590778db3588397f6aa
Francois Michaut <f.michaut@orange.fr>
2023-02-10 15:49:27 +0100

    recuperation des users de la bdd et envoi vers le front

--- assets/components/Users.js
+++ assets/components/Users.js
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
-
+import axios from 'axios';
 
 class Users extends Component {
+    constructor() {
+        super();
+        this.state = { users: [], loading: true};
+    }
+    
+    componentDidMount() {
+        this.getUsers();
+    }
+    
+    getUsers() {
+       axios.get(`http://localhost:8000/api/users`).then(users => {
+            console.log(users.data);
+           this.setState({ users: users.data, loading: false})
+       })
+    }
     render() {
         return (
             <div>
@@ -10,30 +25,23 @@ class Users extends Component {
                     <thead>
                         <tr>
                         <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Firstname</th>
+                        <th scope="col">Email</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                        </tr>
+                        { this.state.users.map( user => 
+                            <tr key={user.id}>
+                                <td scope="row">{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.firstname}</td>
+                                <td>@{user.email}</td>
+                            </tr>
+
+                            )}
+                       
+                        
                     </tbody>
                 </table>
             </div>
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Repository\UsersRepository;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class DefaultController extends AbstractController
 {
@@ -14,5 +19,28 @@ class DefaultController extends AbstractController
     public function index()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/api/users", name="api_users")
+     */
+    public function getUsers(UsersRepository $repository, PersistenceManagerRegistry $doctrine, SerializerInterface $serializer): JsonResponse
+    {
+        $repository = $doctrine->getRepository(Users::class);
+
+        $users = $repository->findAll();
+
+        //    dump($users);
+        $jsonContent = $serializer->serialize($users, 'json');
+
+        $response = new JsonResponse();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin',  '*');
+
+        $response->setContent($jsonContent);
+
+
+        return $response;
     }
 }
