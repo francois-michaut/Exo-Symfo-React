e3cc44c84aa38abefdeeffb4ab5628b369d9859e
Francois Michaut <f.michaut@orange.fr>
2023-02-10 16:09:39 +0100

    Ajout du bouton supprimer et lien sur le nom pour chaque ligne

--- assets/components/Users.js
+++ assets/components/Users.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Users extends Component {
     constructor() {
@@ -13,7 +14,6 @@ class Users extends Component {
     
     getUsers() {
        axios.get(`http://localhost:8000/api/users`).then(users => {
-            console.log(users.data);
            this.setState({ users: users.data, loading: false})
        })
     }
@@ -28,15 +28,21 @@ class Users extends Component {
                         <th scope="col">Name</th>
                         <th scope="col">Firstname</th>
                         <th scope="col">Email</th>
+                        <th scope="col">Adresse</th>
+                        <th scope="col">Téléphone</th>
+                        <th scope="col">Supprimer</th>
                         </tr>
                     </thead>
                     <tbody>
                         { this.state.users.map( user => 
                             <tr key={user.id}>
                                 <td scope="row">{user.id}</td>
-                                <td>{user.name}</td>
+                                <td><Link className={"nav-link"} to={`/details/${user.id}`}>{user.name}</Link></td>
                                 <td>{user.firstname}</td>
-                                <td>@{user.email}</td>
+                                <td>{user.email}</td>
+                                <td>{user.adresse}</td>
+                                <td>{user.telephone}</td>
+                                <td><button type="submit">X</button></td>
                             </tr>
 
                             )}
